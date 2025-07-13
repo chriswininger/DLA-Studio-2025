b049002afdb220d6d928b4fa1fbd717d3e96c27a
chris <chris@pop-os.localdomain>
2025-07-12 22:27:58 -0400

    building out svg page

--- src/tabs/SVGDLA/SVGDLA.tsx
+++ src/tabs/SVGDLA/SVGDLA.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 const SVGDLA: React.FC = () => {
+  const CANVAS_WIDTH = 500;
+  const CANVAS_HEIGHT = 500;
+
   return (
     <div style={{ textAlign: 'center', marginTop: '3rem' }}>
-      <h2>SVG DLA (Placeholder)</h2>
-      <p>This is a placeholder for the SVG DLA experiment tab.</p>
+      <h2>SVG DLA</h2>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
+        <svg
+          width={CANVAS_WIDTH}
+          height={CANVAS_HEIGHT}
+          style={{ 
+            border: '1px solid #ccc', 
+            background: '#111',
+            display: 'block'
+          }}
+          viewBox={`0 0 ${CANVAS_WIDTH} ${CANVAS_HEIGHT}`}
+        >
+          {/* SVG content will go here */}
+        </svg>
+      </div>
+      <div style={{ marginTop: '1rem' }}>
+        <button onClick={() => console.log('Generate SVG clicked')}>
+          Generate SVG
+        </button>
+      </div>
     </div>
   );
 };
