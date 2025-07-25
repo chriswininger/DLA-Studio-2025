6716939a4b329982bc1185505585cae5a27572e7
chris <chris.wininger@gmail.com>
2025-07-25 10:11:07 -0400

    prepping for publication

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,19 @@ import DistanceGradient from './tabs/DistanceGradient/distance-gradient';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/DLA-Studio-2025">
+      {/* <RedirectHandler /> */}
       <div>
         <nav className="dlasim_tab-nav">
-          <NavLink to="/simple-2d-animated-dla" className={({ isActive }) => `dlasim_tab-link${isActive ? ' dlasim_active' : ''}`}>Simple DLA</NavLink>
-          <NavLink to="/distance-gradient" className={({ isActive }) => `dlasim_tab-link${isActive ? ' dlasim_active' : ''}`}>Distance Gradient</NavLink>
-          <NavLink to="/svg-dla" className={({ isActive }) => `dlasim_tab-link${isActive ? ' dlasim_active' : ''}`}>SVG DLA</NavLink>
+          <NavLink to="/simple-2d-animated-dla" className={({ isActive }) => `dlasim_tab-link${isActive ? ' dlasim_active' : ''}`}>
+            Simple DLA (TEST)
+          </NavLink>
+          <NavLink to="/distance-gradient" className={({ isActive }) => `dlasim_tab-link${isActive ? ' dlasim_active' : ''}`}>
+            Distance Gradient
+            </NavLink>
+          <NavLink to="/svg-dla" className={({ isActive }) => `dlasim_tab-link${isActive ? ' dlasim_active' : ''}`}>
+            SVG DLA
+          </NavLink>
         </nav>
         <Routes>
           <Route path="/" element={<Navigate to="/simple-2d-animated-dla" replace />} />
