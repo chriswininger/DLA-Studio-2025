7a58844cb085a6561aaa0fc2eaa25f06e6153c65
chris <chris.wininger@gmail.com>
2025-09-11 15:43:11 -0400

    Added radio button for controlling spawn type

--- src/tabs/Simple2DAnimatedDLA/paint-brush-controls/paint-brush-controls.tsx
+++ src/tabs/Simple2DAnimatedDLA/paint-brush-controls/paint-brush-controls.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../../../store';
-import { setBrushSize, setBrushParticles } from '../simple-2d-animated-dla-slice';
+import { setBrushSize, setBrushParticles, setBrushSpawnType } from '../simple-2d-animated-dla-slice';
 import type { RootState } from '../../../store';
 import type { Simple2DAnimatedDLAUIState } from '../simple-2d-animated-dla-slice';
 import './paint-brush-controls.css';
@@ -19,6 +19,9 @@ const PaintBrushControls: React.FC<PaintBrushControlsProps> = ({
   );
   const brushParticles = useAppSelector((state: RootState) => 
     (state.simple2dAnimatedDla as Simple2DAnimatedDLAUIState).brushParticles
+  );
+  const brushSpawnType = useAppSelector((state: RootState) => 
+    (state.simple2dAnimatedDla as Simple2DAnimatedDLAUIState).brushSpawnType
   );
 
   return (
@@ -51,6 +54,34 @@ const PaintBrushControls: React.FC<PaintBrushControlsProps> = ({
           className="dlasim_paint-input"
         />
       </div>
+      <div className="dlasim_paint-row">
+        <div className="dlasim_radio-group">
+          <label className="dlasim_radio-label">
+            <input
+              type="radio"
+              name="brush-spawn-type"
+              value="walkers"
+              checked={brushSpawnType === 'walkers'}
+              onChange={handleBrushSpawnTypeChange}
+              disabled={isRunning}
+              className="dlasim_radio-input"
+            />
+            Walkers
+          </label>
+          <label className="dlasim_radio-label">
+            <input
+              type="radio"
+              name="brush-spawn-type"
+              value="stuck points"
+              checked={brushSpawnType === 'stuck points'}
+              onChange={handleBrushSpawnTypeChange}
+              disabled={isRunning}
+              className="dlasim_radio-input"
+            />
+            Stuck Points
+          </label>
+        </div>
+      </div>
     </div>
   );
 
@@ -66,6 +97,11 @@ const PaintBrushControls: React.FC<PaintBrushControlsProps> = ({
     if (!isNaN(val) && val > 0) {
       dispatch(setBrushParticles(val));
     }
+  }
+
+  function handleBrushSpawnTypeChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value as 'walkers' | 'stuck points';
+    dispatch(setBrushSpawnType(value));
   }
 };
 
